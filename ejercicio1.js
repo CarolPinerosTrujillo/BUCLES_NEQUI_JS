33f4d08b76b4146439bde0c9975519fa7c26d878
CarolPinerosTrujillo <caroll25m@gmail.com>
2026-07-07 10:01:44 -0500

    inicio repo ejercicios bucles

---
+++ ejercicio1.js
@@ -0,0 +1,9 @@
+const movimientos =[-100, 100, 200, -250, 500, 700]
+
+let total = 0;
+let cantidadRetiros = 0;
+
+for (let i = 0; i < movimientos.length; i++) {
+    const element = array[index];
+    
+}
